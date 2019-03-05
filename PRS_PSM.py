017cebc2d4280ad66768615c3e304c2557b10c03
Sarat Moka <sarathmoka@gmail.com>
2019-03-05 13:33:54 +1000

    Added both python codes

--- PRS_PSM.py
+++ PRS_PSM.py
@@ -5,9 +5,10 @@ Created on Thu Feb 22 12:55:29 2018
 @author: uqsbabum
 """
 
+
 # Discrete PRS alg to generate samples from Penetrable Spheres Mixture Model on [0,1]^2 with parameter kappa_1, Kappa_2 and interaction range,
-# and abosolutely continuous with respect to 
-# the Poisson point process with intensity Kappa = (Kappa_1+Kappa_2)/(pi*IntRange*IntRange), where IntRange is 
+# and abosolutely continuous with respect to
+# the Poisson point process with intensity Kappa = (Kappa_1+Kappa_2)/(pi*IntRange*IntRange), where IntRange is
 # the interaction range of the model. Note that type i points in PPP have intensity Kappa_i/(pi*IntRange*IntRange).
 
 def reset():
@@ -26,7 +27,7 @@ import datetime
 
 
 
-###### Euclidean distance between two points on R^2 
+###### Euclidean distance between two points on R^2
 def euclidean2dim(yy, xx):
     return np.sqrt((xx[0] - yy[0])**2 + (xx[1] - yy[1])**2)
 
@@ -36,9 +37,9 @@ def Overlap(A, B, IntRange):
         for y in B:
             if euclidean2dim(x, y) < IntRange:
                 return True
-    return False  
+    return False
 
-###### Subroutine returns True if NC is within the Interaction range from an other type point in A. Used in dCFTP function. 
+###### Subroutine returns True if NC is within the Interaction range from an other type point in A. Used in dCFTP function.
 def isBlocked(A, NC, IntRange):
     B = [x for x in A if x[2] != NC[2]]
     for x in B:
@@ -55,11 +56,11 @@ def dCFTP(Cell_no, Kappa1, Kappa2, IntRange):
     Cell_y_len = min(IntRange, 1 - y_cord*IntRange)
 
     Kappa_cell = Cell_x_len*Cell_y_len*np.divide(4*(Kappa1 + Kappa2), np.pi*IntRange*IntRange)
-    
-    D = [] 
+
+    D = []
     events = [] #Records the events happened
     gamma = Kappa1/(Kappa1 + Kappa2)
-    D.append([(Cell_x_len*np.random.random_sample(), Cell_y_len*np.random.random_sample(), np.random.binomial(1, gamma)) for i in range(np.random.poisson(Kappa_cell))]) # Intial state 
+    D.append([(Cell_x_len*np.random.random_sample(), Cell_y_len*np.random.random_sample(), np.random.binomial(1, gamma)) for i in range(np.random.poisson(Kappa_cell))]) # Intial state
 
     D.append(D[0][:])
     no_cirD = len(D[0]) #Number of disks at time 0
@@ -70,12 +71,12 @@ def dCFTP(Cell_no, Kappa1, Kappa2, IntRange):
         D[1].append(NewCen)
         no_cirD = no_cirD + 1
         events.append((-1, NewCen))
-        
+
     else:
         k = np.random.randint(no_cirD) ## Index of the departuring element
         vv = D[1].pop(k)
         events.append((1,vv))
-        no_cirD = no_cirD - 1 
+        no_cirD = no_cirD - 1
 
     n = 1
     while True: ## This while Loop ends only when there is a coalscence
@@ -89,33 +90,33 @@ def dCFTP(Cell_no, Kappa1, Kappa2, IntRange):
                     v_index = L.index(events[n - j - 1][1])
                 except:
                     v_index = -1
-                    
+
                 if v_index != -1:
                     del L[v_index]
 
-                
+
                 try:
                     v_index = U.index(events[n - j - 1][1])
                 except:
                     v_index = -1
-                
+
                 if v_index != -1:
                     del U[v_index]
-                    
-                
+
+
             else:
 #                    print("Arrival update of U and L")
 
                 Block_in_U = isBlocked(U, events[n - j - 1][1], IntRange)
                 Block_in_L = isBlocked(L, events[n - j - 1][1], IntRange)
 #                    print((OL_ind_U, OL_ind_L))
-                
+
                 if Block_in_U == False:
                     L.append(events[n - j - 1][1])
                     U.append(events[n - j - 1][1])
-                    
+
                 elif Block_in_L == False:
-                    U.append(events[n - j - 1][1])                        
+                    U.append(events[n - j - 1][1])
 
             if (set(L) <= set(U)) == False:
                 print("L is not a subset of U")
@@ -123,40 +124,40 @@ def dCFTP(Cell_no, Kappa1, Kappa2, IntRange):
         ## Terminating the loop when U == L
         if len(L) == len(U):
             break
-        
-        
+
+
         ## Loop for doubling the length of the dominated process
-        for j in range(n): 
+        for j in range(n):
             D.append(D[n+j][:])
-            
+
             if  no_cirD == 0 or np.random.binomial(1, np.divide(Kappa_cell, Kappa_cell + no_cirD)) == 1: # True if it is an arrival
 
                 NewCen = (Cell_x_len*np.random.random_sample(), Cell_y_len*np.random.random_sample(), np.random.binomial(1, gamma))
                 D[n+j+1].append(NewCen)
                 no_cirD = no_cirD + 1
                 events.append((-1, NewCen))
-                
+
             else:
                 k = np.random.randint(0, no_cirD) ## Index of the departuring element
                 vv = D[n+j+1].pop(k)
-                events.append((1,vv))                    
-                no_cirD = no_cirD - 1 
-        
-        n = 2*n 
-        
+                events.append((1,vv))
+                no_cirD = no_cirD - 1
+
+        n = 2*n
+
     State = []
     for pt in L:
         State.append((pt[0] + x_cord*IntRange, pt[1] + y_cord*IntRange, pt[2]))
-    
+
     return State
- 
+
 
 ###### Node of the dependency graph
 class GNode:
     def __init__(self, initdata):
         self.event = initdata
         self.next = None
-        
+
 ###### Node for keeping a neighbor of a given node of the dependency graph
 class NNode:
     def __init__(self, initdata):
@@ -164,7 +165,7 @@ class NNode:
         self.next = None
 
 
-##### Subroutine for generating the nodes of the dependency graph    
+##### Subroutine for generating the nodes of the dependency graph
 def generateVertices(K):
     stime = time.time()
     head = None
@@ -175,17 +176,17 @@ def generateVertices(K):
         x1 = int(x/K)
         for y0 in range(max(0, x0-1), min(K, x0+2)):
             for y1 in range(max(0, x1-1), min(K, x1+2)):
-                
+
                 Vert.add(frozenset((x, y1*K+y0)))
         Vert.remove(frozenset((x,x)))
-        
+
     for a in Vert:
         pt = GNode(list(a))
         pt.next = head
         head = pt
-        
+
     print("\n Vertices of the dependency graph is generated. Time taken: ", int(time.time() - stime), "secs")
-                
+
     return head
 
 
@@ -199,7 +200,7 @@ def generateNeighbors(K):
 
         Nb.remove(x)
 
-        
+
         for y in Nb:
             head = NNode(y)
             head.next = Neighbors[x]
@@ -210,22 +211,22 @@ def generateNeighbors(K):
 ## New Partial Rejection Sampling Alg
 def PSM_NPRS(K, Kappa1, Kappa2, IntRange, Graph, Neighbors):
 
-    
-    Disks1 = [[] for _ in range(K*K)] 
-    Disks2 = [[] for _ in range(K*K)] 
-    BadCells = [False for _ in range(K*K)]   
-    
+
+    Disks1 = [[] for _ in range(K*K)]
+    Disks2 = [[] for _ in range(K*K)]
+    BadCells = [False for _ in range(K*K)]
+
     ## Intial state generation
     for x in range(K*K):
         State = dCFTP(x, Kappa1, Kappa2, IntRange)
         Disks1[x] = [x[0:2] for x in State if x[2] == 1]
         Disks2[x] = [x[0:2] for x in State if x[2] == 0]
-    
+
     # Start of the while loop of Partial Rejection Sampling Method
     count_while = 0
     while True:
         count_while = count_while + 1
-    
+
         ## Identifying the bad events
         stop = True
         pt = Graph
@@ -239,7 +240,7 @@ def PSM_NPRS(K, Kappa1, Kappa2, IntRange, Graph, Neighbors):
                 BadCells[y] = True
 
             pt = pt.next
-    
+
         ## PRS is terminated if there are no bad events
         if stop:
             break
@@ -258,33 +259,33 @@ def PSM_NPRS(K, Kappa1, Kappa2, IntRange, Graph, Neighbors):
                             stop = False
                             ResCells[y] = True
                         current = current.next
-                        
-                      
-                        
+
+
+
         for x in range(K*K):
             if (len(Disks1[x]) > 0 or len(Disks1[x]) > 0) and ResCells[x]:
                 current = Neighbors[x]
                 while current != None:
                     ResCells[current.neighbor] = True
                     current = current.next
-                        
-        
+
+
         for x in range(K*K):
             if ResCells[x]:
                 State = dCFTP(x, Kappa1, Kappa2, IntRange)
                 Disks1[x] = [x[0:2] for x in State if x[2] == 1]
                 Disks2[x] = [x[0:2] for x in State if x[2] == 0]
-                        
-            BadCells[x] = False
-                
 
-    ## Conclusions after the iteration                       
+            BadCells[x] = False
+
+
+    ## Conclusions after the iteration
     npoints1 = 0
     npoints2 = 0
     for x in range(K*K):
         npoints1 = npoints1 + len(Disks1[x])
         npoints2 = npoints2 + len(Disks2[x])
-        
+
     return (npoints1, npoints2, count_while)
 
 
@@ -327,14 +328,14 @@ Neighbors = generateNeighbors(K)
 
 for b in range(Itot):
 #    print('---------- New PRS Iteration: ', b+1, '---------')
-    
-    
-    (count_pts1, count_pts2, count_while) = PSM_NPRS(K, Kappa1, Kappa2, IntRange, Graph, Neighbors)          
+
+
+    (count_pts1, count_pts2, count_while) = PSM_NPRS(K, Kappa1, Kappa2, IntRange, Graph, Neighbors)
     npoints1 = np.divide(b, b + 1)*npoints1 + np.divide(count_pts1, b + 1)
     npoints2 = np.divide(b, b + 1)*npoints2 + np.divide(count_pts2, b + 1)
     Exp_while_loops = np.divide(b, b + 1)*Exp_while_loops + np.divide(count_while, b + 1)
-    
-    
+
+
 #
 #    print(' Exp no of type 1 points :', npoints1)
 #    print(' Exp no of type 2 points :', npoints2)
@@ -354,9 +355,5 @@ print('Time Taken: ', Est_time)
 ##plt.plot(Beta, Est_time)
 ##plt.xlabel('Intensity of the Poisson point process')
 ##plt.ylabel('Time taken to generate 10,000 samples (in secs)')
-##plt.show()        
+##plt.show()
 #
-
-
-
-     
--- PRS_Strauss.py
+++ PRS_Strauss.py
@@ -5,8 +5,9 @@ Created on Thu Feb 22 12:55:29 2018
 @author: uqsbabum
 """
 
-# Program to generate Strauss process on [0,1]^2 with parameter Gamma and abosolutely continuous with respect to 
-# the Poisson point process with intensity Kappa = Kappa_0/(pi*IntRange*IntRange), where IntRange is 
+
+# Program to generate Strauss process on [0,1]^2 with parameter Gamma and abosolutely continuous with respect to
+# the Poisson point process with intensity Kappa = Kappa_0/(pi*IntRange*IntRange), where IntRange is
 # the interaction range of the model.
 
 
@@ -26,7 +27,7 @@ import math
 
 
 
-###### Euclidean distance between two points on R^2 
+###### Euclidean distance between two points on R^2
 def euclidean2dim(yy, xx):
     return np.sqrt((xx[0] - yy[0])**2 + (xx[1] - yy[1])**2)
 
@@ -36,7 +37,7 @@ def Overlap(A, B, IntRange):
         for y in B:
             if euclidean2dim(x, y) < IntRange:
                 return True
-    return False  
+    return False
 
 ###### Subroutine to count the number of overlaps the points in A have with the points in B
 def OverlapCount(A, B, IntRange):
@@ -45,13 +46,13 @@ def OverlapCount(A, B, IntRange):
         for y in B:
             if  euclidean2dim(x, y) < IntRange:
                 count = count + 1
-            
+
     return count
 
 ###### Subroutine to find the indecies of the circles in A that are blocking the new circle NC. Used in dCFTP function. See Huber's swap based dCFTP for details
 def BlockInd(A, NC, IntRange):
     Ind = []
-    
+
     for i in range(len(A)):
         if euclidean2dim(A[i][0:2], NC[0:2]) < IntRange and A[i][2] == 0:
             Ind.append(i)
@@ -67,10 +68,10 @@ def dCFTP_swap(Cell_no, Kappa, gamma, IntRange):
     Cell_y_len = min(IntRange, 1 - y_cord*IntRange)
 
     Kappa_cell = Kappa*Cell_x_len*Cell_y_len
-    
-    D = [] 
+
+    D = []
     events = [] #Records the events happened
-    D.append([(Cell_x_len*np.random.random_sample(), Cell_y_len*np.random.random_sample(), np.random.binomial(1, gamma)) for i in range(np.random.poisson(Kappa_cell))]) # Intial state 
+    D.append([(Cell_x_len*np.random.random_sample(), Cell_y_len*np.random.random_sample(), np.random.binomial(1, gamma)) for i in range(np.random.poisson(Kappa_cell))]) # Intial state
 
     D.append(D[0][:])
     no_cirD = len(D[0]) # Number of disks at time 0
@@ -83,12 +84,12 @@ def dCFTP_swap(Cell_no, Kappa, gamma, IntRange):
         no_cirD = no_cirD + 1
         events.append((-1, NewCen))
 
-        
+
     else:
         k = np.random.randint(no_cirD) ## Index of the departuring element
         vv = D[1].pop(k)
         events.append((1,vv))
-        no_cirD = no_cirD - 1 
+        no_cirD = no_cirD - 1
 
     n = 1
 
@@ -102,61 +103,61 @@ def dCFTP_swap(Cell_no, Kappa, gamma, IntRange):
                     v_index = L.index(events[n - j - 1][1])
                 except:
                     v_index = -1
-                    
+
                 if v_index != -1:
                     del L[v_index]
 
-                
+
                 try:
                     v_index = U.index(events[n - j - 1][1])
                 except:
                     v_index = -1
-                
+
                 if v_index != -1:
                     del U[v_index]
-                
+
             else:
                 Block_Ind_U = BlockInd(U, events[n - j - 1][1], IntRange)
                 Block_Ind_L = BlockInd(L, events[n - j - 1][1], IntRange)
-                
+
                 if len(U) == 0 or len(Block_Ind_U) == 0:
                     L.append(events[n - j - 1][1])
                     U.append(events[n - j - 1][1])
-                    
+
                 elif len(Block_Ind_U) == 1:
 
                     try:
                         v_index = L.index(U[Block_Ind_U[0]])
                     except:
                         v_index = -1
-                        
-                    if v_index != -1:
-                        del L[v_index]                        
 
-                    del U[Block_Ind_U[0]]                        
-                    
+                    if v_index != -1:
+                        del L[v_index]
+
+                    del U[Block_Ind_U[0]]
+
                     L.append(events[n - j - 1][1])
-                    U.append(events[n - j - 1][1])                        
-                    
+                    U.append(events[n - j - 1][1])
+
                 elif len(Block_Ind_L) == 0:
                     U.append(events[n - j - 1][1])
-                    
-                    
+
+
                 elif len(Block_Ind_L) == 1:
 
-                    del L[Block_Ind_L[0]]                        
+                    del L[Block_Ind_L[0]]
                     U.append(events[n - j - 1][1])
 
             if (set(L) <= set(U)) == False:
                 print("L is not a subset of U")
                 exit()
-        
+
         if len(L) == len(U):
             break
 
         for j in range(n): #Loop for doubling the length of the dominated process
             D.append(D[n+j][:])
-            
+
             if  no_cirD == 0 or np.random.binomial(1, np.divide(Kappa_cell, Kappa_cell + no_cirD)) == 1: # True if it is an arrival
                 NewCen = (Cell_x_len*np.random.random_sample(), Cell_y_len*np.random.random_sample(), np.random.binomial(1, gamma))
                 D[n+j+1].append(NewCen)
@@ -165,15 +166,15 @@ def dCFTP_swap(Cell_no, Kappa, gamma, IntRange):
             else:
                 k = np.random.randint(0, no_cirD) ## Index of the departuring element
                 vv = D[n+j+1].pop(k)
-                events.append((1,vv))                    
-                no_cirD = no_cirD - 1 
-        
-        n = 2*n            
+                events.append((1,vv))
+                no_cirD = no_cirD - 1
+
+        n = 2*n
 
     State = []
     for pt in L:
         State.append((pt[0] + x_cord*IntRange, pt[1] + y_cord*IntRange))
-        
+
     return State
 
 
@@ -183,14 +184,14 @@ class GNode:
         self.event = initdata
         self.uniform = 0
         self.next = None
-        
+
 ###### Node for keeping a neighbor of a given node of the dependency graph
 class NNode:
     def __init__(self, initdata):
         self.neighbor = initdata
         self.next = None
 
-##### Generating the nodes of the dependency graph    
+##### Generating the nodes of the dependency graph
 def generateVertices(K):
     stime = time.time()
     head = None
@@ -199,18 +200,18 @@ def generateVertices(K):
         x0 = x%K
         x1 = int(x/K)
         for y0 in range(max(0, x0-1), min(K, x0+2)):
-            for y1 in range(max(0, x1-1), min(K, x1+2)):        
+            for y1 in range(max(0, x1-1), min(K, x1+2)):
                 Vert.add(frozenset((x, y1*K+y0)))
-                
+
         Vert.remove(frozenset((x,x)))
-        
+
     for a in Vert:
         pt = GNode(list(a))
         pt.next = head
         head = pt
-        
+
     print("\n Vertices of the dependency graph is generated. Time taken: ", int(time.time() - stime), "secs")
-                
+
     return head
 
 
@@ -223,7 +224,7 @@ def generateNeighbors(K):
         Nb = [j*K+i for i in range(max(0, x0-1), min(K, x0+2)) for j in range(max(0, x1-1), min(K, x1+2))]
 
         Nb.remove(x)
-        
+
         for y in Nb:
             head = NNode(y)
             head.next = Neighbors[x]
@@ -233,25 +234,25 @@ def generateNeighbors(K):
 
 ## New Partial Rejection Sampling Alg
 def straussNPRS(K, Kappa, gamma, IntRange, Graph, Neighbors):
-    
-    Disks = [[] for _ in range(K*K)] 
-    BadCells = [False for _ in range(K*K)]   
-    
+
+    Disks = [[] for _ in range(K*K)]
+    BadCells = [False for _ in range(K*K)]
+
     ## Intial state generation
     for x in range(K*K):  # x is the cell number
         Disks[x] = dCFTP_swap(x, Kappa, gamma, IntRange)
-        
-    ## Generate uniform random variables associated with each event/edge 
+
+    ## Generate uniform random variables associated with each event/edge
     pt = Graph
     while pt != None:
         pt.uniform = np.random.random_sample()
         pt = pt.next
-    
+
     # Start of the while loop of Partial Rejection Sampling Method
     count_while = 0
     while True:
         count_while = count_while + 1
-    
+
         ## Identifying the bad events
         stop = True
         pt = Graph
@@ -260,14 +261,14 @@ def straussNPRS(K, Kappa, gamma, IntRange, Graph, Neighbors):
             (x,y) = list(pt.event)
 
             count_overlaps = OverlapCount(Disks[x], Disks[y], IntRange)
-                
+
             if pt.uniform > np.power(gamma, count_overlaps):
                 stop = False
                 BadCells[x] = True
                 BadCells[y] = True
 
             pt = pt.next
-    
+
         ## PRS is terminated if there is no bad event
         if stop:
             break
@@ -287,35 +288,35 @@ def straussNPRS(K, Kappa, gamma, IntRange, Graph, Neighbors):
                             stop = False
                             ResCells[y] = True
                         current = current.next
-                        
+
         pt = Graph
         while pt != None:
             (x,y) = list(pt.event)
             if ResCells[x] == True or ResCells[y] == True:
                 pt.uniform = np.random.random_sample()
-            pt = pt.next                        
-                        
+            pt = pt.next
+
         for x in range(K*K):
             if len(Disks[x]) and ResCells[x]:
                 current = Neighbors[x]
                 while current != None:
                     ResCells[current.neighbor] = True
                     current = current.next
-                        
-        
+
+
         for x in range(K*K):
             if ResCells[x]:
-                Disks[x] = dCFTP_swap(x, Kappa, gamma, IntRange)                
-                        
-            BadCells[x] = False
-                
+                Disks[x] = dCFTP_swap(x, Kappa, gamma, IntRange)
 
-    ## Conclusions after the iteration                       
+            BadCells[x] = False
+
+
+    ## Conclusions after the iteration
     npoints = 0
     for x in range(K*K):
         npoints = npoints + len(Disks[x])
     result = (npoints, count_while)
-    
+
     return result
 
 ###### Model parameters
@@ -353,13 +354,13 @@ Neighbors = generateNeighbors(K)
 
 for b in range(Itot):
     print('---------- New PRS Iteration: ', b+1, '---------')
-    
-    
-    (count_pts, count_while) = straussNPRS(K, Kappa, gamma, IntRange, Graph, Neighbors)          
+
+
+    (count_pts, count_while) = straussNPRS(K, Kappa, gamma, IntRange, Graph, Neighbors)
     npoints = np.divide(b, b + 1)*npoints + np.divide(count_pts, b + 1)
     Exp_while_loops = np.divide(b, b + 1)*Exp_while_loops + np.divide(count_while, b + 1)
-    
-    
+
+
 
     print(' Exp no of points :', npoints)
     print(' Expected while loops :', Exp_while_loops)
@@ -372,7 +373,3 @@ print('Results:')
 print('  Estimated # points:', npoints)
 print('  Expected # while loops:', Exp_while_loops)
 print('Program Ending time: ', datetime.datetime.now().time())
-
-
-
-     
